8bdc7de5a1a6e6cc774a7c7aec23e10faea2a71f
Danielle Marie <dhutchings36@yahoo.com>
2019-08-01 23:11:00 -0500

    added styling and categories

--- script.js
+++ script.js
@@ -21,22 +21,26 @@ function displayResults(responseJson) {
   $('#results-list').empty();
   $('#similar-venues').empty();
   $('#more-venues').addClass('hidden');
+  $('#more-venues').removeClass('backdrop');
   $('#restaurant-location').empty();
   $('#restaurant-map').addClass('hidden');
+  $('#restaurant-map').removeClass('backdrop');
   for (let i = 0; i < responseJson.response.venues.length; i++){
     $('#results-list').append(
-     `<li><button type="submit" class="restaurantChoice" data-id="${responseJson.response.venues[i].id}" data-filter="${responseJson.response.venues[i].location.address},${responseJson.response.venues[i].location.city}+${responseJson.response.venues[i].location.state}" data-name="${responseJson.response.venues[i].name} ">${responseJson.response.venues[i].name}</button>
+     `<li><button type="submit" class="restaurantChoice" id="resChoice "data-id="${responseJson.response.venues[i].id}" data-filter="${responseJson.response.venues[i].location.address},${responseJson.response.venues[i].location.city}+${responseJson.response.venues[i].location.state}" data-name="${responseJson.response.venues[i].name} ">${responseJson.response.venues[i].name}</button>
       <p>${responseJson.response.venues[i].location.address}</p>
       <p>${responseJson.response.venues[i].categories[0].name}</p>
       </li>
     `)};
   $('#results').removeClass('hidden');
+  $('#results').addClass('backdrop');
 };
 
 //function for restaurant selected to pull up directions and similar venues
 function watchSection() {
     $('section').on('click','.restaurantChoice', function (event) {
       event.preventDefault(findIdURL, mapURL);
+      $('#results').removeClass('backdrop');
       const venueId = $(this).attr('data-id');
       const venueAddress = $(this).attr('data-filter');
       const venueName = $(this).attr('data-name');
@@ -59,11 +63,12 @@ function getSimilarVenues(responseJson) {
     $('#results').addClass('hidden')
     for (let i = 0; i < responseJson.response.similarVenues.items.length; i++){
     $('#similar-venues').append(
-       `<li><h3>${responseJson.response.similarVenues.items[i].name}</h3>
+       `<li><button class="similarVenueChoice" id="simVenChoice">${responseJson.response.similarVenues.items[i].name}</button>
        <p>${responseJson.response.similarVenues.items[i].location.address}</p>
        <p>${responseJson.response.similarVenues.items[i].categories[0].name}</p></li>
       `)};
     $('#more-venues').removeClass('hidden');
+    $('#more-venues').addClass('backdrop');
 };
 
 //function to add google map with location of restaurant clicked
@@ -75,10 +80,11 @@ function getRestaurantLocationMap(updatedMapURL, venueName) {
     </iframe>`
   );
  $('#restaurant-map').removeClass('hidden');
+ $('#restaurant-map').addClass('backdrop');
 };
 
 //function to pull restaurants based off of city input
-function getRestaurantList(query, limit) {
+function getRestaurantList(query, limit, foodType) {
   const params = {
     client_id: CLIENT_ID,
     client_secret: CLIENT_SECRET,
@@ -86,7 +92,7 @@ function getRestaurantList(query, limit) {
     v: 20180323,
     limit: limit,
     intent: 'browse',
-    categoryId: '4d4b7105d754a06374d81259',
+    categoryId: foodType,
   };
 
   //building url with search query
@@ -147,9 +153,21 @@ function watchForm() {
     event.preventDefault();
     const citySearch = $('#js-city-search').val();
     const limit = $('#js-limit').val();
-    getRestaurantList(citySearch, limit);
+    const foodType = $('#js-category-food').val();
+    console.log(foodType);
+    getRestaurantList(citySearch, limit, foodType);
   });
 }
+
+
+//slider javascript
+var slider = document.getElementById("js-limit");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+};
 
 
 $(watchForm);
